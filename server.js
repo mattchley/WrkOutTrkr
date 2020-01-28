6fa962bfb93c03320daab4e02c0ac80401b031ee
mattchley <mattchley@gmail.com>
2020-01-28 15:32:53 -0800

    GetFunct,HyperlinksFunct

--- server.js
+++ server.js
@@ -18,6 +18,37 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
 
 
+// routes
+// GET "/api/workouts"
+// PUT "/api/workouts/:id"
+// POST "/api/workouts"
+// GET "/api/workouts/range"
+
+
+// Post exercise
+
+app.post("/api/workouts", (req, res) => {
+  db.Workout.insert(req.body, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+// Get Workouts
+
+app.get("/api/workouts", (req, res) => {
+  db.Workout.find({}, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(data);
+    }
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
